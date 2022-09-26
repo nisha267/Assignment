3dae5216e6262011eb11290f5e3faf464357bd95
Nisha Sutradhar <111556178+nisha267@users.noreply.github.com>
2022-09-26 11:58:28 +0530

    Add files via upload

---
+++ Multiply.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class Multiply {// Java Program to Multiply Two Floating-Point Numbers
+
+	public static void main(String args[]) {
+		float a, b;
+		a = 3.8f;
+		b = 6.7f;
+		float c = a * b;
+		System.out.println("The multiplication of two numbers is " + c);
+	}
+}
