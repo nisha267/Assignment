cb99cf98ae7799306157d701b03bf674fbe2010d
Nisha Sutradhar <111556178+nisha267@users.noreply.github.com>
2022-09-26 12:08:24 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Multiplication {
+	public static void main(String args[]) {
+		// Java Program to Multiply two Numbers
+		int a, b;
+		a = 45;
+		b = 34;
+		System.out.println("The multiplication of two number " + (a * b));
+	}
+}
