e0d6c5ff1dc120fd0cadb4903167b10a0d3f07e3
Nisha Sutradhar <111556178+nisha267@users.noreply.github.com>
2022-09-26 13:10:30 +0530

    Add files via upload

---
+++ Prime_no.java
@@ -0,0 +1,20 @@
+package demo;
+
+public class Prime_no {
+
+	public static void main(String args[]) {
+		int n = 7;
+		int count = 0;
+		int i = 2;
+		while (i <= n / 2) {
+			if (n % i == 0) {
+				count++;
+			}
+			i++;
+		}
+		if (count > 0)
+			System.out.println("The no is not a prime no");
+		else
+			System.out.println("The no is  a prime no");
+	}
+}
