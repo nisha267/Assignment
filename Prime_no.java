3135a4d04852e511879f69ec6bf4ca4cf0cee4e2
Nisha Sutradhar <111556178+nisha267@users.noreply.github.com>
2022-09-26 13:23:20 +0530

    Update Prime_no.java

--- Prime_no.java
+++ Prime_no.java
@@ -6,13 +6,14 @@ public class Prime_no {
 		int n = 7;
 		int count = 0;
 		int i = 2;
+		//condition to know the number is prime or not
 		while (i <= n / 2) {
 			if (n % i == 0) {
 				count++;
 			}
 			i++;
 		}
-		if (count > 0)
+		if (count > 0) 
 			System.out.println("The no is not a prime no");
 		else
 			System.out.println("The no is  a prime no");
