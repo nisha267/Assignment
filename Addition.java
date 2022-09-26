f7279b8fe3fb700d70456868b12987b2fc517b60
Nisha Sutradhar <111556178+nisha267@users.noreply.github.com>
2022-09-26 12:02:34 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,12 @@
+package demo;
+
+//Java Program to Add two Numbers
+public class Addition {
+	public static void main(String args[]) {
+		int a, b;
+		a = 56;
+		b = 67;
+		float c = a + b;
+		System.out.println("The addition of two numbers is " + c);
+	}
+}
